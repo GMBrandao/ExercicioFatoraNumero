9569452d3b0911942a2ee7b19e04186e0c886afa
GBrandao04 <gabrielmartbrandao@gmail.com>
2023-03-06 12:05:46 -0300

    Add project files.

---
+++ FatoraNumero/Program.cs
@@ -0,0 +1,20 @@
+﻿long result, factor;
+
+Console.WriteLine("Digite o número que deseja fatorar: ");
+factor = long.Parse(Console.ReadLine()); ;
+
+result = fat(factor);
+
+Console.WriteLine("O resultado da fatorial é: " +  result);
+
+long fat(long value)
+{
+    if (value == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return(value * fat(value-1));
+    }
+}
